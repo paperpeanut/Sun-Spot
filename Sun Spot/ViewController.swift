19bfc562f5894f76a7f2a619e410eeee51a242e8
Christy <christy.daly@me.com>
2018-06-02 12:48:20 -0400

    Initial Commit

---
+++ Sun Spot/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Sun Spot
+//
+//  Created by Christy Daly on 6/2/18.
+//  Copyright © 2018 Christy Daly. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
